87a03f6b36ffacd63c7813fe52569c6d2bec68c6
René Porzelt <rene.tube992@gmail.com>
2024-09-25 21:21:30 +0200

    up to date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatProgressSpinnerModule],
+  imports: [RouterOutlet, MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
