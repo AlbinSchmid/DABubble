c5f1e3762d321249180d9dab499bb6f7abf358a3
René Porzelt <rene.tube992@gmail.com>
2024-09-26 21:12:12 +0200

    added privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MessengerComponent } from './dashboard/messenger/messenger.component';
+import { PrivacyPolicyComponent } from './shared/privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
   { path: 'dashboard', component: MessengerComponent },
   { path: '', component: LandingPageComponent },
+  { path: 'privacy-policy', component: PrivacyPolicyComponent },
   { path: 'imprint', component: ImprintComponent }
 ];
--- src/app/shared/privacy-policy/privacy-policy.component.ts
+++ src/app/shared/privacy-policy/privacy-policy.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule,
+    RouterLink,
+    MatIconModule
+  ],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
