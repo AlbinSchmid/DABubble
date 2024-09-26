5ba0e23ecaace5b194720557af238f7afe50c00a
René Porzelt <rene.tube992@gmail.com>
2024-09-26 13:01:02 +0200

    added in style css the primary and secondary style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRippleModule } from '@angular/material/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatRippleModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
