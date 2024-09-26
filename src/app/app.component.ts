0fd28859fa9f08235aef15149157de94ddf83c8b
René Porzelt <rene.tube992@gmail.com>
2024-09-26 13:05:05 +0200

    forgot remove imports of mat burron module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatButtonModule } from '@angular/material/button';
-import { MatRippleModule } from '@angular/material/core';
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    MatButtonModule,
-    MatRippleModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
