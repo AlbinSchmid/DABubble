9d0d62644ff15b16ee4721d4abfa0368d10912f9
NaviPlau <naviplau@outlook.com>
2024-09-26 12:41:47 +0200

    folders arrangement

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingLoginDialogComponent } from "../landing-login-dialog/landing-login-dialog.component";
+import { LandingLoginDialogComponent } from "./landing-login-dialog/landing-login-dialog.component";
 
 @Component({
   selector: 'app-landing-page',
