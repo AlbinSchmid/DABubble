e2139bb301e51d18de2804282669b793363991bc
NaviPlau <naviplau@outlook.com>
2024-09-26 10:33:56 +0200

    delete test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatProgressSpinnerModule],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
