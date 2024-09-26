461d1e54cbc19c0d3bcb0649c80f1d1537927e37
Albin Schmid <schmidalbin15@gmail.com>
2024-09-26 12:44:28 +0200

    added start-animation component

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingLoginDialogComponent } from "./landing-login-dialog/landing-login-dialog.component";
+import { StartAnimationComponent } from './start-animation/start-animation.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingLoginDialogComponent],
+  imports: [CommonModule, RouterOutlet, LandingLoginDialogComponent, StartAnimationComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
