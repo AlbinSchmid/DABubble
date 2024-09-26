2210db9dce3688f096b2daa17af71b88ace96c5e
NaviPlau <naviplau@outlook.com>
2024-09-26 19:25:40 +0200

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ImprintComponent } from './shared/imprint/imprint.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MessengerComponent } from './dashboard/messenger/messenger.component';
 
 export const routes: Routes = [
+  { path: 'dashboard', component: MessengerComponent },
   { path: '', component: LandingPageComponent },
-  { path: 'landing-login-dialog', component: LandingPageComponent },
   { path: 'imprint', component: ImprintComponent }
 ];
