b7f24d337b56480eeaee588167cc96f6603ecd13
NaviPlau <naviplau@outlook.com>
2024-09-26 21:39:25 +0200

    update

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,16 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LandingLoginDialogComponent } from "./landing-login-dialog/landing-login-dialog.component";
 import { StartAnimationComponent } from './start-animation/start-animation.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingLoginDialogComponent, StartAnimationComponent],
+  imports: [CommonModule, RouterOutlet, LandingLoginDialogComponent, StartAnimationComponent, RouterLink, RouterLinkActive],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
-
+  
 }
