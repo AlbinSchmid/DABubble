fbb91b391961a861002c31118bd6ecf2539f5ffd
Albin Schmid <schmidalbin15@gmail.com>
2024-09-30 20:35:40 +0200

    added detail-person component and finished the design of this component

--- src/app/dashboard/messenger/messenger.component.ts
+++ src/app/dashboard/messenger/messenger.component.ts
@@ -1,8 +1,9 @@
-import { AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
+import { DetailPersonComponent } from './detail-person/detail-person.component';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-messenger',
@@ -11,14 +12,18 @@ import { CdkScrollable, ScrollDispatcher } from '@angular/cdk/scrolling';
     MatDividerModule,
     MatButtonModule,
     MatIconModule,
+    DetailPersonComponent,
+    MatDialogModule,
   ],
   templateUrl: './messenger.component.html',
   styleUrl: './messenger.component.scss'
 })
 export class MessengerComponent {
+  dialog = inject(MatDialog);
 
-  show(event: any) {
-    console.log(event);
-    
+  openDialog() {
+    this.dialog.open(DetailPersonComponent, {
+      panelClass: 'my-custom-dialog'
+    });
   }
 }
