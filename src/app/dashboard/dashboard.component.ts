08f17915e4a429f027e39446a19bd5e768a2eb44
René Porzelt <rene.tube992@gmail.com>
2024-10-02 15:51:47 +0200

    add sive nav toggle btn without logic

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,10 @@ import { HeaderComponent } from './header/header.component';
 import { ChannelsUserlistComponent } from './channels-userlist/channels-userlist.component';
 import { MessengerComponent } from './messenger/messenger.component';
 import { ThreadComponent } from './thread/thread.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -11,7 +15,10 @@ import { ThreadComponent } from './thread/thread.component';
     HeaderComponent,
     ChannelsUserlistComponent,
     MessengerComponent,
-    ThreadComponent
+    ThreadComponent,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
