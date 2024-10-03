bcc5b28a9b89830da284ccf211885859c14893d7
René Porzelt <rene.tube992@gmail.com>
2024-10-03 16:43:30 +0200

    improved logo in header and start devspace card

--- src/app/dashboard/channels-userlist/channels-userlist.component.ts
+++ src/app/dashboard/channels-userlist/channels-userlist.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-channels-userlist',
   standalone: true,
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatIconModule
   ],
   templateUrl: './channels-userlist.component.html',
   styleUrl: './channels-userlist.component.scss'
