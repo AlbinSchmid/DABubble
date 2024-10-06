60e5fa3a258ab3c7c9bdabaf7d3860b834a1adb4
René Porzelt <rene.tube992@gmail.com>
2024-10-06 19:52:48 +0200

    fix icons in dashboard

--- src/app/dashboard/channels-userlist/channels-userlist.component.ts
+++ src/app/dashboard/channels-userlist/channels-userlist.component.ts
@@ -1,17 +1,20 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTreeModule } from '@angular/material/tree';
+
 
 @Component({
   selector: 'app-channels-userlist',
   standalone: true,
   imports: [
     MatCardModule,
-    MatIconModule
+    MatIconModule, MatTreeModule
   ],
   templateUrl: './channels-userlist.component.html',
   styleUrl: './channels-userlist.component.scss'
 })
 export class ChannelsUserlistComponent {
 
+  channels: string[] = ['Entwicklerteam', 'UI-Team', 'SecOps-Team']
 }
