fd1a607a2d86c109b96d05bdbda7437e8a8e7312
René Porzelt <rene.tube992@gmail.com>
2024-10-09 17:20:29 +0200

    set a user status in user interface

--- src/app/landing-page/interfaces/userinterface.ts
+++ src/app/landing-page/interfaces/userinterface.ts
@@ -1,7 +1,8 @@
 export interface UserInterface {
   userID: string,
   password: string,
-  email:string;
-  username:string;
-  avatar: string ;
+  email: string;
+  username: string;
+  avatar: string;
+  userStatus?: string;
 }
