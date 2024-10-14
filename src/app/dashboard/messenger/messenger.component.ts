22e7737fea898594924193f6300e35e6aac0884c
Albin Schmid <schmidalbin15@gmail.com>
2024-10-14 20:51:49 +0200

    added emojis to input field can use it now

--- src/app/dashboard/messenger/messenger.component.ts
+++ src/app/dashboard/messenger/messenger.component.ts
@@ -1,11 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DetailPersonComponent } from './detail-person/detail-person.component';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { timestamp } from 'rxjs';
 import { ThreadService } from '../../shared/services/thread-service/thread.service';
 import { FirebaseMessengerService } from '../../shared/services/firebase-services/firebase-messenger.service';
@@ -37,16 +37,100 @@ export class MessengerComponent {
   hoveredMessage: number;
   hoveredMenu = false;
   unsubChatList;
+  dateCount = 0;
+  showEmoijs = false;
 
-  constructor(public firebaseMessenger: FirebaseMessengerService, public threadService: ThreadService, public messengerService: MessengerService) {
+
+
+
+
+  normalEmojis: string[] = [
+    '😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😊',
+    '😋', '😎', '😍', '😗', '😙', '🙂', '🤗', '🤔', '😐', '😑', 
+    '😶', '🙄', '😏', '😣', '😥', '😮', '🤐', '😯', '😪', '😫',
+    '😴', '😌', '😜', '🤤', '😛', '🤑', '😲', '🙃', '😷', '🤒', 
+    '🤕', '🤧', '😵', '🤯', '😤', '😭', '😢', '😨'
+  ];
+
+  workEmojis: string[] = [
+    '💼', '📁', '📅', '🖥️', '📊', '📈', '📉', '📝', '💻', '🖱️',
+    '📋', '📌', '🖇️', '📄', '✏️', '📤', '📥', '📧', '📞', '📡', 
+    '🔒', '🔓', '🗑️', '🧾', '📆', '🏢', '🏛️'
+  ];
+
+  
+  activeBoard: string = 'normal';
+  selectedEmoji: string | null = null;
+
+  @Output() emojiSelected = new EventEmitter<string>();
+
+  
+  selectEmoji(emoji: string, inputField: HTMLTextAreaElement): void {
+    this.selectedEmoji = emoji;
+    
+    const start = inputField.selectionStart ?? 0;
+    const end = inputField.selectionEnd ?? 0;
+
+    this.firebaseMessenger.content = this.firebaseMessenger.content.slice(0, start) + emoji + this.firebaseMessenger.content.slice(end);
+    
+    
+    setTimeout(() => {
+      inputField.selectionStart = inputField.selectionEnd = start + emoji.length;
+      inputField.focus();
+    }, 0);
+  }
+
+  switchBoard(board: string): void {
+    this.activeBoard = board;
+  }
+
+
+  openEmoijs() {
+    if (this.showEmoijs == false) {
+      this.showEmoijs = true;
+    } else {
+      this.showEmoijs = false;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  constructor(public firebaseMessenger: FirebaseMessengerService, public threadService: ThreadService, public messengerService: MessengerService, public datePipe: DatePipe) {
     this.unsubChatList = firebaseMessenger.subChatsList();   
   }
 
+
+  checkDate(messageDate: Date) {
+    const formatter = new Intl.DateTimeFormat('de-DE', { dateStyle: 'short' });
+    let dateToday = new Date();
+    const dateTodayString = formatter.format(dateToday);
+    const dateMessageString = formatter.format(messageDate);
+    
+    if (dateTodayString == dateMessageString) {
+      return 'Heute';
+    } else {
+      return this.datePipe.transform(messageDate, 'EEEE, MMMM d');
+    }
+  }
+
+  
   ngOnDestroy() {
     this.unsubChatList;
   }
 
 
+  /**
+   * We controll if the interaction menu should be hide or not
+   */
   showOrHideMenu() {
     if (this.hoveredMenu == false) {
       this.hoveredMenu = true;
@@ -56,6 +140,9 @@ export class MessengerComponent {
   }
 
 
+  /**
+   * Open the detail View of the channel or person
+   */
   openDialog() {
     this.dialog.open(DetailPersonComponent, {
       panelClass: 'my-custom-dialog'
