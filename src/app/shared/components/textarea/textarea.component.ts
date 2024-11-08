8ba8d04caca85090ed4a7569d283f954468efdbf
Albin Schmid <schmidalbin15@gmail.com>
2024-11-08 17:38:47 +0100

    work on mention a user in the textarea

--- src/app/shared/components/textarea/textarea.component.ts
+++ src/app/shared/components/textarea/textarea.component.ts
@@ -48,6 +48,7 @@ export class TextareaComponent {
 
   usersListAll: UserInterface[] = [];
   usersToMention: MentionUserInterface[] = [];
+  filteredUsersToMention: MentionUserInterface[] = [];
   alreadyMentionUsers: MentionUserInterface[] = [];
   selectedFiles: any[] = [];
 
@@ -60,18 +61,40 @@ export class TextareaComponent {
   messenger = 'messenger';
   mentionPersonView = false;
   showEmojiBoard = false;
-  items: [
-    { name: 'item1', value: 'Item 1' },
-    { name: 'item2', value: 'Item 2' },
-    { name: 'item3', value: 'Item 3' }
-  ];
 
   mentionConfig = {
+    labelKey: 'userName',
+    items: [] as MentionUserInterface[],
     mentionChar: "@",
+    mentionDialogOpened: false,
   }
+
+  mentionActive: boolean = false;
 
 
   constructor(private dialog: MatDialog, private storage: Storage) {
+  }
+  
+
+
+  onMentionActivate(event: Event) {
+    if (!this.mentionConfig.mentionDialogOpened) {
+      console.log('Hello');
+      
+    }
+  }
+  
+  openMentionDialog() {
+    // const dialogRef = this.dialog.open(MentionDialogComponent, {
+    //   data: {
+    //     users: this.mentionConfig.items
+    //   }
+    // });
+  
+    // dialogRef.afterClosed().subscribe(result => {
+    //   console.log('Der Dialog wurde geschlossen', result);
+    // });
+    // this.mentionConfig.mentionDialogOpened = true;
   }
 
 
@@ -89,6 +112,59 @@ export class TextareaComponent {
   }
 
 
+  checkForMention(event: Event): void {
+
+
+
+
+
+    // const inputElement = this.firebaseMessenger.content;
+
+    // // // Aktiviert Mention, wenn nach einem @-Zeichen ein Leerzeichen eingegeben wurde
+    // // if (/\B@\w*$/.test(inputElement) && !this.mentionActive) {
+    // //   console.log("Mention aktiviert: Ein '@' am Anfang eines Wortes wurde eingegeben.");
+    // //   this.openOrCloseMentionPersonView();
+    // //   console.log(this.usersToMention);
+
+
+    // //   this.mentionActive = true; // Mention aktivieren
+    // //   this.mentionPersonView = true;
+    // // }
+
+    // if (/\B@\w*$/.test(inputElement)) { // Falls ein @ am beginn eines Wortes steht      
+    //   let txtSearchUser = inputElement.match(/@(\S+)$/); // txtSearchUser is TRUE wenn ein zeichen nach dem @ sich befindet
+    //   console.log(txtSearchUser);
+      
+    //   this.filteredUsersToMention = this.usersToMention;
+      
+      
+    //   if (!this.mentionActive) {
+    //     console.log("Mention aktiviert: Ein '@' am Anfang eines Wortes wurde eingegeben.");
+    //     this.mentionActive = true; // Mention aktivieren
+    //   }
+      
+    //   if (txtSearchUser) {
+    //     console.log(this.filteredUsersToMention);
+    //     let lowerCaseSearchUser = txtSearchUser[1].toLowerCase();
+    //     this.filteredUsersToMention = this.usersToMention.filter(user => user.userName.toLowerCase().startsWith(lowerCaseSearchUser));
+    //     this.mentionPersonView = true;
+    //   } 
+    // } else {
+    //   this.mentionPersonView = false;
+    // }
+
+    // // Deaktiviert Mention, wenn nach einem @-Zeichen ein Leerzeichen eingegeben wurde
+    // if (this.mentionActive && /\B@\w*\s$/.test(inputElement)) {
+    //   console.log("Mention beendet wegen Leerzeichen.");
+    //   this.openOrCloseMentionPersonView();
+    //   this.mentionActive = false; // Mention deaktivieren
+    //   this.mentionPersonView = false;
+    // }
+
+
+  }
+
+
   subChannelList() {
     const messegeRef = doc(collection(this.firestore, `channels`), this.messengerService.channel.channelID);
     return onSnapshot(messegeRef, (list) => {
@@ -101,7 +177,11 @@ export class TextareaComponent {
           this.usersToMention.push(this.getCleanJson(user));
           this.usersToMention = this.usersToMention.filter(user => user.userID !== this.authService.currentUserSig()?.userID);
         }
-        this.sortByName(this.usersToMention);        
+        this.sortByName(this.usersToMention);
+        this.usersToMention.forEach((user) => {
+          this.mentionConfig.items.push(user);
+        });
+
       } else {
         console.error("doc is empty or doesn't exist");
       }
@@ -147,9 +227,19 @@ export class TextareaComponent {
   mentionUser(userJson: any) {
     this.usersToMention = this.usersToMention.filter(user => user.userID !== userJson.userID);
     this.alreadyMentionUsers.push(userJson);
-    console.log(this.alreadyMentionUsers);
-    
-    this.openOrCloseMentionPersonView();
+
+    // const lastAtIndex = this.firebaseMessenger.content.lastIndexOf('@');
+    // let txtSearchUser = this.firebaseMessenger.content.match(/@(\S+)/); // Hier bekommen wir den Text der Hinter dem @ steht!!
+    // if (!txtSearchUser) {
+    //   // this.firebaseMessenger.content = this.firebaseMessenger.content.substring(0, lastAtIndex) + `@${userJson.userName}` + this.firebaseMessenger.content.substring(lastAtIndex + 1);
+    //   this.firebaseMessenger.content = this.firebaseMessenger.content.replace(/@[^@]*$/, `@${userJson.userName}`);
+    // } else {
+    //   // this.firebaseMessenger.content = this.firebaseMessenger.content.replace(/@[\w-]+/, `@${userJson.userName}`);
+    // }
+
+    // this.filteredUsersToMention = this.usersToMention;
+
+    // this.openOrCloseMentionPersonView();
   }
 
 
@@ -292,7 +382,7 @@ export class TextareaComponent {
   private updateContent(messenger: any, originalContent: string) {
     if (messenger === 'messenger') {
       this.firebaseMessenger.content = originalContent;
-      this.firebaseMessenger.createMessage('noID', 'noCollection' ,this.alreadyMentionUsers);
+      this.firebaseMessenger.createMessage('noID', 'noCollection', this.alreadyMentionUsers);
     } else {
       this.firebaseMessenger.answerContent = originalContent;
       this.firebaseMessenger.createMessage(this.threadService.messageToReplyTo.messageID, 'answer', false);
