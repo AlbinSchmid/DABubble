63cbfdcbf540c3d5baeff28c6dc410a0bd6390a9
René Porzelt <rene.tube992@gmail.com>
2024-11-14 12:48:25 +0100

    start stlye new message component

--- src/app/dashboard/new-message/new-message.component.ts
+++ src/app/dashboard/new-message/new-message.component.ts
@@ -1,13 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { TextareaComponent } from '../../shared/components/textarea/textarea.component';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-new-message',
   standalone: true,
   imports: [
     CommonModule,
-    TextareaComponent
+    TextareaComponent,
+    MatInputModule
   ],
   templateUrl: './new-message.component.html',
   styleUrl: './new-message.component.scss'
