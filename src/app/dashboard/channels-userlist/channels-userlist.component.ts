7193270b3dd7be1ea12c9ec2b6c91aff3d790974
René Porzelt <rene.tube992@gmail.com>
2024-11-29 13:48:32 +0100

    removed a import component in channels- userlist component

--- src/app/dashboard/channels-userlist/channels-userlist.component.ts
+++ src/app/dashboard/channels-userlist/channels-userlist.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -6,6 +6,7 @@ import { UserListComponent } from './user-list/user-list.component';
 import { ChannelListComponent } from './channel-list/channel-list.component';
 import { MessengerService } from '../../shared/services/messenger-service/messenger.service';
 import { ViewportService } from '../../shared/services/viewport.service';
+import { SearchService } from '../../shared/services/search-service/search.service';
 
 @Component({
   selector: 'app-channels-userlist',
@@ -21,10 +22,25 @@ import { ViewportService } from '../../shared/services/viewport.service';
   styleUrls: ['./channels-userlist.component.scss']
 })
 export class ChannelsUserlistComponent {
+  @ViewChild('searchInput') searchInput!: ElementRef;
 
   messengerService: MessengerService = inject(MessengerService);
   viewportService: ViewportService = inject(ViewportService);
+  searchService: SearchService = inject(SearchService);
 
   constructor() { }
 
+  /**
+ * Focuses the search input element to allow the user to type a search query.
+ */
+  focusSearchInput(): void {
+    this.searchInput.nativeElement.focus();
+  }
+
+  /**
+ * Handles the search functionality by passing the user's input to the SearchService.
+ */
+  onSearch(): void {
+    this.searchService.search(this.searchInput.nativeElement.value);
+  }
 }
