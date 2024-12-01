87349d6a412c0b33bcbda364341fcc987e1fd3ed
Albin Schmid <schmidalbin15@gmail.com>
2024-12-01 14:13:22 +0100

    messenger and thread should be now full responsive without the channels

--- src/app/dashboard/thread/thread.component.ts
+++ src/app/dashboard/thread/thread.component.ts
@@ -25,7 +25,7 @@ import { ViewportService } from '../../shared/services/viewport.service';
   templateUrl: './thread.component.html',
   styleUrl: './thread.component.scss'
 })
-export class ThreadComponent implements AfterViewInit{
+export class ThreadComponent implements AfterViewInit {
   viewportService = inject(ViewportService)
   reduceInteraktionBtn = true;
   editAnswerMessage = true;
@@ -33,18 +33,81 @@ export class ThreadComponent implements AfterViewInit{
   @ViewChild('content') scrollContainer: ElementRef;
   parser = inject(MessageParserService);
 
+  @ViewChild('headerName') headerName: ElementRef;
+  @ViewChild('messageToReplayName') messageToReplayName: ElementRef;
+  isTextWrapped = false;
+  isTextWrappedMessage = false;
+  resizeObserverHeader!: ResizeObserver;
+  resizeObserverMessage!: ResizeObserver;
+  checkTextSenderName = false;
+  checkTextSenderNameMessage = false;
+  windowWith: number;
+  windowWithMessage: number;
+  headerSenderName: string;
+  messageToReplaySenderName: string;
+
 
   constructor(public threadService: ThreadService, public firebaseMessenger: FirebaseMessengerService, public messengerService: MessengerService) {
   }
-  
-  
+
+
+  ngOnInit() {
+    this.headerSenderName = this.threadService.messageToReplyTo.senderName;
+    this.messageToReplaySenderName = this.threadService.messageToReplyTo.senderName;
+  }
+
+
   ngAfterViewInit() {
-      this.threadService.scrollContainer = this.scrollContainer;
+    this.threadService.scrollContainer = this.scrollContainer;
+    this.resizeObserverMessage = new ResizeObserver(() => this.checkTextStatusTest());
+    this.resizeObserverHeader = new ResizeObserver(() => this.checkTextStatus());
+    this.resizeObserverHeader.observe(this.headerName.nativeElement);
+    this.resizeObserverMessage.observe(this.messageToReplayName.nativeElement);
+  }
+
+  checkTextStatusTest() {
+    const element = this.messageToReplayName.nativeElement;
+    const lineHeight = parseFloat(getComputedStyle(element).lineHeight || '0');
+    const headerNameHeight = element.offsetHeight;
+
+    this.isTextWrappedMessage = headerNameHeight > lineHeight;
+    if (this.isTextWrappedMessage && !this.checkTextSenderName) {
+      this.windowWithMessage = this.viewportService.width;
+      this.checkTextSenderNameMessage = true;
+      this.messageToReplaySenderName = `${this.messengerService.getFirstWord(this.threadService.messageToReplyTo.senderName)}. ${this.messengerService.getSecondWordFirstLetter(this.threadService.messageToReplyTo.senderName)}`;
+    } else if (this.windowWithMessage < this.viewportService.width) {
+      this.messageToReplaySenderName = this.threadService.messageToReplyTo.senderName;
+      this.checkTextSenderNameMessage = false;
+    }
+  }
+
+
+  checkTextStatus() {
+    const element = this.headerName.nativeElement;
+    console.log('header');
+    
+    const lineHeight = parseFloat(getComputedStyle(element).lineHeight || '0');
+    const headerNameHeight = element.offsetHeight;
+    this.isTextWrapped = headerNameHeight > lineHeight;
+    if (this.isTextWrapped && !this.checkTextSenderName) {
+      this.windowWith = this.viewportService.width;
+      this.checkTextSenderName = true;
+      this.headerSenderName = `${this.messengerService.getFirstWord(this.threadService.messageToReplyTo.senderName)}. ${this.messengerService.getSecondWordFirstLetter(this.threadService.messageToReplyTo.senderName)}`;
+    } else if (this.windowWith < this.viewportService.width) {
+      this.headerSenderName = this.threadService.messageToReplyTo.senderName;
+      this.checkTextSenderName = false;
+    }
   }
 
 
   ngOnDestroy() {
     this.parser.parseMessage(this.firebaseMessenger.content);
+    if (this.resizeObserverHeader) {
+      this.resizeObserverHeader.disconnect();
+    }
+    if (this.resizeObserverMessage) {
+      this.resizeObserverMessage.disconnect();
+    }
   }
 
 
